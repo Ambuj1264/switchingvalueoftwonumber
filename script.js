0f4cca6ea90c604406d43c1b03c8af8545a999a2
AMBUJ SINGH <82210881+Ambuj1264@users.noreply.github.com>
2022-10-12 08:11:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// ==================  1  =====================================
+// var a=10;
+// var b=20;
+//  var c;
+//  c=a;
+// a=b;//10
+//  b=c;//20
+//  console.log("this is value of a:",a);//20
+//  console.log("this is value of b:",b);//10
+
+
+//===================  2  ==================================
+// var a=10,b=20;
+// a=a+b;//a=30 (10+20)    
+// b=a-b;//b=10 (30-20)    
+// a=a-b;//a=20 (30-10)    
+// console.log("this is value of a:",a);
+//  console.log("this is value of b:",b);
+ //=================== 3  =====================================
+
+//  var a=10,b=20;
+//  a=a+b-(b=a/* 10 */);//20
+//  console.log("this is value of a:",a);
+//  console.log("this is value of b:",b);
+ 
+ 
+
+
+// =================== 4 ===========================================
+
+// var a=10,b=20;
+// [a,b]=[b,a]
+// console.log("this is value of a:",a);
+//  console.log(b);
